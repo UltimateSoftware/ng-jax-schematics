123aaf5cc89f067d7fcd4bbd86b7a93dc1bec18e
Jo Lares <jo_lares@ultimatesoftware.com>
2019-05-21 17:49:41 -0400

    JACK-000 (feat): Create jack-schematic's class-test

--- schematics/src/class-test/index.ts
+++ schematics/src/class-test/index.ts
@@ -2,8 +2,8 @@ import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function schematicStarter(_options: any): Rule {
+export function classTest(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    return tree.create("foo.html", "<h1>Hello World!</h1>");
+    return tree.create("class.spec.ts", "describe('Boom shakalaka', () => {})");
   };
 }
