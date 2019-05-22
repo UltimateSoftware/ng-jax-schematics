3cf50612ddfaa469eb88f3272137a897b49dc8ef
Alicia Rodriguez Taboada <alicia_rodriguez@ultimatesoftware.com>
2019-05-21 21:32:19 -0400

    feat: add class-test options & read given file

--- sample-projects/tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ sample-projects/tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { Hero }         from '../hero';
-import { HeroService }  from '../hero.service';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: [ './hero-detail.component.css' ]
+  styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
@@ -25,16 +25,29 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
+    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
   }
 
   goBack(): void {
     this.location.back();
   }
 
- save(): void {
-    this.heroService.updateHero(this.hero)
-      .subscribe(() => this.goBack());
+  save(): void {
+    this.sample2() ? this.goBack() : this.getHero();
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+  }
+
+  sample(): boolean {
+    if (true) {
+      return false;
+    }
+  }
+
+  sample2(): boolean {
+    if (false) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
--- schematics/src/class-test/index.ts
+++ schematics/src/class-test/index.ts
@@ -1,9 +1,62 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import * as ts from 'typescript';
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  SchematicsException,
+  source,
+} from '@angular-devkit/schematics';
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function classTest(_options: any): Rule {
+interface ClassTestOptions {
+  filePath: string;
+}
+export function classTest(_options: ClassTestOptions): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    return tree.create('class.spec.ts', `describe('Boom shakalaka', () => {})`);
+    console.log('options!', _options);
+    const fileBuffer = tree.read(_options.filePath);
+    if (!fileBuffer) {
+      throw new SchematicsException('Not a valid file');
+    }
+    // console.log('fileBuffer!', fileBuffer);
+
+    const fileText = fileBuffer.toString('utf-8');
+    // console.log('file', fileText);
+    const sourceFile = ts.createSourceFile(
+      _options.filePath,
+      fileText,
+      ts.ScriptTarget.Latest,
+      true
+    );
+    // console.log('file', sourceFile);
+
+    delintNode(sourceFile);
   };
+}
+
+function delintNode(node: ts.Node) {
+  switch (node.kind) {
+    case ts.SyntaxKind.IfStatement:
+      const ifStatement = node as ts.IfStatement;
+      console.log('FOUND YOU!!', ifStatement);
+      break;
+      // if (ifStatement.thenStatement.kind !== ts.SyntaxKind.Block) {
+      //   report(
+      //     ifStatement.thenStatement,
+      //     'An if statement\'s contents should be wrapped in a block body.'
+      //   );
+      // }
+      // if (
+      //   ifStatement.elseStatement &&
+      //   ifStatement.elseStatement.kind !== ts.SyntaxKind.Block &&
+      //   ifStatement.elseStatement.kind !== ts.SyntaxKind.IfStatement
+      // ) {
+      //   report(
+      //     ifStatement.elseStatement,
+      //     'An else statement\'s contents should be wrapped in a block body.'
+      //   );
+      // }
+      break;
+  }
+
+  ts.forEachChild(node, delintNode);
 }
