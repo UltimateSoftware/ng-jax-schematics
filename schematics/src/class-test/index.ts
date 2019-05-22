b38af191ae3df61bd86ae2dc4bd949309de959f3
Alicia Rodriguez Taboada <alicia_rodriguez@ultimatesoftware.com>
2019-05-22 10:33:03 -0400

    feat: identify reading method names & conditionals

--- schematics/src/class-test/index.ts
+++ schematics/src/class-test/index.ts
@@ -1,62 +1,67 @@
 import * as ts from 'typescript';
-import {
-  Rule,
-  SchematicContext,
-  Tree,
-  SchematicsException,
-  source,
-} from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, SchematicsException } from '@angular-devkit/schematics';
 
 interface ClassTestOptions {
   filePath: string;
 }
+
+// filepath: src/app/hero-detail/hero-detail.component.ts
 export function classTest(_options: ClassTestOptions): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    console.log('options!', _options);
     const fileBuffer = tree.read(_options.filePath);
     if (!fileBuffer) {
       throw new SchematicsException('Not a valid file');
     }
-    // console.log('fileBuffer!', fileBuffer);
 
     const fileText = fileBuffer.toString('utf-8');
-    // console.log('file', fileText);
     const sourceFile = ts.createSourceFile(
       _options.filePath,
       fileText,
       ts.ScriptTarget.Latest,
       true
     );
-    // console.log('file', sourceFile);
 
-    delintNode(sourceFile);
+    getMethodNames.findMethodNames.bind(getMethodNames)(sourceFile);
+    const methodNames = getMethodNames.methodNames.filter((n) => n !== '');
+    console.log('getMethodNames.methodNames', methodNames);
+
+    getConditionals.findConditionals.bind(getConditionals)(sourceFile);
+    console.log('getConditionals.ifStatementCount', getConditionals.ifStatementCount);
+    console.log('getConditionals.ternaryStatementCount', getConditionals.ternaryStatementCount);
+    console.log('getConditionals.totalCount', getConditionals.totalCount);
   };
 }
 
-function delintNode(node: ts.Node) {
-  switch (node.kind) {
-    case ts.SyntaxKind.IfStatement:
-      const ifStatement = node as ts.IfStatement;
-      console.log('FOUND YOU!!', ifStatement);
-      break;
-      // if (ifStatement.thenStatement.kind !== ts.SyntaxKind.Block) {
-      //   report(
-      //     ifStatement.thenStatement,
-      //     'An if statement\'s contents should be wrapped in a block body.'
-      //   );
-      // }
-      // if (
-      //   ifStatement.elseStatement &&
-      //   ifStatement.elseStatement.kind !== ts.SyntaxKind.Block &&
-      //   ifStatement.elseStatement.kind !== ts.SyntaxKind.IfStatement
-      // ) {
-      //   report(
-      //     ifStatement.elseStatement,
-      //     'An else statement\'s contents should be wrapped in a block body.'
-      //   );
-      // }
-      break;
-  }
+const getMethodNames = {
+  methodNames: [''],
+  findMethodNames: function(node: ts.Node): any {
+    switch (node.kind) {
+      case ts.SyntaxKind.MethodDeclaration:
+        const methodDeclaration = node as ts.MethodDeclaration;
+        const nameIdentifier = methodDeclaration.name as ts.Identifier;
+        this.methodNames = [...this.methodNames, nameIdentifier.escapedText as string];
+        break;
+    }
+    ts.forEachChild(node, this.findMethodNames.bind(this));
+  },
+};
 
-  ts.forEachChild(node, delintNode);
-}
+const getConditionals = {
+  ifStatementCount: 0,
+  ternaryStatementCount: 0,
+  totalCount: 0,
+  findConditionals: function(node: ts.Node) {
+    switch (node.kind) {
+      case ts.SyntaxKind.IfStatement:
+        this.ifStatementCount += 1;
+        break;
+      case ts.SyntaxKind.ConditionalExpression:
+        this.ternaryStatementCount += 1;
+        break;
+    }
+
+    this.totalCount = this.ifStatementCount + this.ternaryStatementCount;
+
+    ts.forEachChild(node, this.findConditionals.bind(this));
+  },
+};
